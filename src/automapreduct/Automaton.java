654e0c528e7bec8c801ae080cec7cdc01cb8fb4f
kondrashov <kondrashov@KONDRASHOV.main.dom>
2015-02-20 12:34:53 +0400

    Rename Finite Deterministic Automaton class to AAutomaton class

---
+++ src/automapreduct/Automaton.java
@@ -0,0 +1,31 @@
+package automapreduct;
+
+
+public class Automaton {
+
+    public Automaton() {
+    
+        
+    
+    }
+
+    
+    
+    
+    private class TransitionOutput {
+
+        public TransitionOutput(int Condition, String Output) {
+            this.Condition = Condition;
+            this.Output = Output;
+        }
+                      
+        int Condition; // Next condition
+        String Output; // Output
+    }
+
+    
+    
+    
+    
+    
+}

802a18a5a673acb74eb1f72e168d5bdb50a51b8b
beardedjack <evgen.kondrashoff@gmail.com>
2015-06-07 18:08:40 +0300

    fix run.cmd

--- run.cmd
+++ run.cmd
@@ -1,2 +1,2 @@
--Xms2048m  -Xmx2048m -Xss2048m -Xmn2048m -XX:PermSize=2048m -XX:MaxPermSize=2048m -verbose:gc -Xloggc:gc.log -XX:+PrintGCTimeStamps -XX:+PrintGCDetails -XX:+DisableExplicitGC
+java -jar dist\AutoMapReduct.jar -Xms2048m  -Xmx2048m -Xss2048m -Xmn2048m -XX:PermSize=2048m -XX:MaxPermSize=2048m -verbose:gc -Xloggc:gc.log -XX:+PrintGCTimeStamps -XX:+PrintGCDetails -XX:+DisableExplicitGC
 pause
